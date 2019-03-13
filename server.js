99521057ba045eb774b2247f5ea1bb085c26faa1
Wonmi <wonmadesign@gmail.com>
2019-03-14 03:17:02 +0900

    Added POST /users/me/ingres route for updating filter ingredients

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const _ = require('lodash')
 const bcrypt = require('bcryptjs') 
 const cors = require('cors')
 const app = express()
-
+app.use(bodyParser.json())
+app.use(cors())
 
 const { ObjectID } = require('mongodb')
 const { mongoose } = require('./db/mongoose')
@@ -13,8 +14,21 @@ const { Recipe } = require('./models/recipe')
 const { User } = require('./models/user')
 const { auth } = require('./middleware/auth')
 
-app.use(bodyParser.json())
-app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: false })) // PATCT PUT 연동위해
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   )
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//     return res.status(200).json({})
+//   }
+//   next()
+// })
+
 
 app.get('/recipes', auth,(req, res) => {
     Recipe.find({
@@ -83,6 +97,33 @@ app.post('/users/login', (req, res) => {
     })
 })
 
+app.post('/users/me/ingres', auth, (req, res) => {
+
+    // const body = _.pick(req.body, ['email', 'password'])    
+    // User.findByCredentials(body.email, body.password).then((user) => {
+    //     return user.generateAuthToken().then((token) => {
+    //         res.send({token, user})
+    //         // res.send(token)
+    //     })
+    // })
+    // .catch((e) => {
+    //     res.status(400).send('no user')
+    // })
+    const filterIngre = req.body.filterIngre.map((ingre) => {
+        return ingre.name
+    })
+    // res.send({babo: 'babo'})
+    User.findOneAndUpdate({
+        _id:req.user._id
+    }, {$set: {filterIngre: filterIngre} }, {new: true}).then((user) => {
+        if(!user) {
+            return res.status(404).send('user not found')
+        }
+        res.send(user.filterIngre)
+    }).catch((e) => {
+        res.status(400).send('Something went wrong in server')
+    })
+})
 
 
 app.get('/', (req, res) => {
